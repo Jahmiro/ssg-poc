133f42213fd8360b7b638edbf4eb6855988fd4b2
Jahmiro <Jahmiro000@hotmail.com>
2024-06-05 10:36:25 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import { GetServerSideProps } from "next";
+import { useState } from "react";
+import Link from "next/link";
+
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+type Props = {
+  data: Pokemon[];
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const res = await fetch(
+    "https://dataset-ssr-ssg.s3.eu-north-1.amazonaws.com/pokemon-main/index.json"
+  );
+
+  if (!res.ok) {
+    console.error("Failed to fetch data:", res.statusText);
+    return {
+      props: {
+        data: [],
+      },
+    };
+  }
+
+  const data: Pokemon[] = await res.json();
+  const currentPokemons = data.slice(0, 12);
+  return {
+    props: {
+      data: currentPokemons,
+    },
+  };
+};
+
+const Page: React.FC<Props> = ({ data }) => {
+  return (
+    <div className="flex flex-col items-center justify-center my-[40px]">
+      <h1 className="text-tertiary-800 text-[28px] w-full text-center font-bold mb-[20px]">
+        Pokemon List
+      </h1>
+      <main className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {data.map((pokemon) => (
+          <Link key={pokemon.id} href={`/pokemon/${pokemon.id}`}>
+            <div className="max-w-sm bg-white border border-neutral-400 rounded-lg shadow hover:border-tertiary-800">
+              <div className="flex items-center justify-center h-48 rounded-t-lg overflow-hidden my-[10%]">
+                <img
+                  className="w-[40%]"
+                  src={`https://dataset-ssr-ssg.s3.eu-north-1.amazonaws.com/pokemon-main/${pokemon.image}`}
+                  alt={pokemon.name}
+                />
+              </div>
+
+              <div className="bg-neutral-150 border-t border-neutral-400 rounded-b-lg">
+                <p className="p-[10px] text-tertiary-800">
+                  {pokemon.id}. {pokemon.name}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </main>
+    </div>
+  );
+};
+
+export default Page;
---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,102 @@
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+
+type Stat = {
+  name: string;
+  value: number;
+};
+
+type Pokemon = {
+  id: number;
+  name: string;
+  image: string;
+  type: string[];
+  stats: Stat[];
+};
+
+type Props = {
+  pokemon: Pokemon | null;
+  error: string | null;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const { id } = context.params!;
+  const res = await fetch(
+    `https://dataset-ssr-ssg.s3.eu-north-1.amazonaws.com/pokemon-main/pokemon/${id}.json`
+  );
+
+  if (!res.ok) {
+    return {
+      props: {
+        pokemon: null,
+        error: `Failed to fetch data: ${res.statusText}`,
+      },
+    };
+  }
+
+  const pokemon: Pokemon = await res.json();
+  return {
+    props: {
+      pokemon,
+      error: null,
+    },
+  };
+};
+
+const PokemonDetail: React.FC<Props> = ({ pokemon, error }) => {
+  const router = useRouter();
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!pokemon) {
+    return <div>Pokemon not found</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="bg-white border border-neutral-400 rounded-lg shadow-lg pt-6 max-w-md w-full">
+        <h1 className="text-tertiary-800 text-2xl font-bold mb-4 text-center">
+          {pokemon.name}
+        </h1>
+        <img
+          src={`https://dataset-ssr-ssg.s3.eu-north-1.amazonaws.com/pokemon-main/${pokemon.image}`}
+          alt={pokemon.name}
+          className="w-1/2 mx-auto mb-4"
+        />
+        <p className="text-center mb-4 text-tertiary-600">
+          Type: {pokemon.type.join(", ")}
+        </p>
+        <div className="bg-neutral-150 border-t border-neutral-400 px-6 py-6">
+          <h2 className="text-xl font-bold mb-2 text-center text-tertiary-800">
+            Stats:
+          </h2>
+          <ul className="space-y-2">
+            {pokemon.stats.map((stat, index) => (
+              <li
+                key={index}
+                className="flex justify-between text-tertiary-800"
+              >
+                <span>{stat.name}</span>
+                <span>{stat.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center mt-4">
+        <button
+          onClick={() => router.back()}
+          className="text-secondary-500 px-4 py-2 rounded-lg hover:bg-blue-700 hover:text-secondary-600"
+        >
+          Terug naar homepagina
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonDetail;
